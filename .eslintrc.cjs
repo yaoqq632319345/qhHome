8d34facb32a4dc7b33d7c25f3714cda860fc05c1
姚头 <632319345@qq.com>
2023-01-04 08:55:59 +0800

    feat(*): ✨ add eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/**
+ * @type { import('eslint').Linter.Config }
+ */
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier', // 自动修复
+    'plugin:prettier/recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    // 3. 注意要加上这一句，开启 prettier 自动修复的功能
+    'prettier/prettier': 'error',
+    'vue/no-v-model-argument': 'off',
+    'vue/no-multiple-template-root': 'off'
+  }
+};
